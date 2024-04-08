d6a0b3c1445370f8caff5b21f9dd207336a03fe1
jparkley <parkleydondon@yahoo.com>
2024-04-08 12:19:21 -0700

    Debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
 import { client, isAuthenticated } from "@/lib/utils/amplify-utils";
-import PostCard from "@/components/posts/PostCard";import { useReducer } from "react";
+import PostCard from "@/components/posts/PostCard";
+import { useReducer } from "react";
 
 export default async function Home() {
-
-  const { data: posts } =  await client.models.Post.list({
+  console.log("🚀 ~ Home ~ client:", client.models);
+  const { data: posts } = await client.models.Post.list({
     selectionSet: ["title", "content", "id", "createdAt"],
     authMode: "apiKey",
-  })
-
+  });
+  console.log("🚀 ~ Home ~ posts:", posts);
   return (
-      <>
-        <div className="container-header">
-          <h2>Share Your Thoughts</h2>
-        </div>
-        <div className="container-middle">
-          {posts && posts.map((post, idx) => (
-              <div className="card" key={idx}>
-                <PostCard
-                  post={post}
-                  isListPage={true}
-                />
-              </div>
+    <>
+      <div className="container-header">
+        <h2>Share Your Thoughts</h2>
+      </div>
+      <div className="container-middle">
+        {posts &&
+          posts.map((post, idx) => (
+            <div className="card" key={idx}>
+              <PostCard post={post} isListPage={true} />
+            </div>
           ))}
-        </div>
-      </>
+      </div>
+    </>
   );
 }
