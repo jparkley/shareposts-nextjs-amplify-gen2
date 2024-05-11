3d450ba0e4bdb915a444d4a0f262fce65ad23df7
jparkley <parkleydondon@yahoo.com>
2024-05-10 21:24:10 -0700

    debugging

--- src/lib/utils/amplify-utils.ts
+++ src/lib/utils/amplify-utils.ts
@@ -4,8 +4,7 @@ import { cookies } from "next/headers";
 import { getCurrentUser } from "aws-amplify/auth/server";
 import { generateServerClientUsingCookies } from "@aws-amplify/adapter-nextjs/data";
 // import { Schema } from "../../amplify/data/resource";
-import { type Schema } from '../../../amplify/data/resource';
-
+import { type Schema } from "../../../amplify/data/resource";
 
 export const client = generateServerClientUsingCookies<Schema>({
   config,
@@ -13,7 +12,7 @@ export const client = generateServerClientUsingCookies<Schema>({
   authMode: "userPool",
 });
 
-
+console.log("====== in amplify-utils ~ client:", client);
 
 export const { runWithAmplifyServerContext } = createServerRunner({
   config,
@@ -25,7 +24,7 @@ export const isAuthenticated = async () =>
     async operation(contextSpec) {
       try {
         const user = await getCurrentUser(contextSpec);
-        console.log('======= user', user)
+        console.log("======= user", user);
         return !!user;
       } catch (error) {
         return false;
